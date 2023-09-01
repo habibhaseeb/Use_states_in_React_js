242bfc2009d34d55146add4ddb18c696eb114878
Habibullah <habibhaseeb716@gmail.com>
2023-09-01 22:29:18 +0500

    usestates

---
+++ App.js
@@ -0,0 +1,28 @@
+@import {usestates} from "react"
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize the state
+    this.state = {
+      count: 0, // Example state variable
+    };
+  }
+
+  // Define a function to update the state
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
